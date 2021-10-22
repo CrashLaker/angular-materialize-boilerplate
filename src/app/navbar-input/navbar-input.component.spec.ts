5dd65aa0fb191f46f5f9bfa61a135911b864d42c
crashlaker <>
2021-10-22 01:12:09 +0000

    hat halloween

---
+++ src/app/navbar-input/navbar-input.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavbarInputComponent } from './navbar-input.component';
+
+describe('NavbarInputComponent', () => {
+  let component: NavbarInputComponent;
+  let fixture: ComponentFixture<NavbarInputComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NavbarInputComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavbarInputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
